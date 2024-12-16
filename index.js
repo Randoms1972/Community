5cabe2e6cdfba10c5c154243fdaa0aca959e7d29
_omg123 <137329925+Randoms1972@users.noreply.github.com>
2024-12-16 18:13:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Watching Hyper Community", "🎮 Online Since 16Th Dec 2024"];
-const statusTypes = [ 'idle', 'test'];
+const statusMessages = ["🎧 Watching Hyper Community", "🎮 Running Since 2024"];
+const statusTypes = [ 'idle', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
